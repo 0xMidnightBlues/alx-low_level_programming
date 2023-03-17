2afc6baf4bce407a3ba0a13a0d488e5eb0d0feac
0xMidnightBlues <ataleb.codes@gmail.com>
2023-03-16 17:43:24 -0700

    added betty

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - prints the phrase followed by a new line
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
 int main(){
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
