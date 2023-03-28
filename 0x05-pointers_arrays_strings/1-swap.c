7879ecfe748c57feff463aaafeb4426bb6db2062
0xMidnightBlues <126298314+0xMidnightBlues@users.noreply.github.com>
2023-03-28 01:26:40 +0000

    Fix

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * swap -  swaps the values of two integers.
+ * swap_int -  swaps the values of two integers.
  * @a: 1st pointer
  * @b: 2nd pointer
  * Return: northing
--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -12,12 +12,8 @@ void print_rev(char *s)
 
 	i = 0;
 	while (s[i] != '\0')
-	{
-		i++
-	}
+		i++;
 	while (s[--i] != '\0')
-	{
 		_putchar(s[i]);
-	}
 	_putchar('\n');
 }
