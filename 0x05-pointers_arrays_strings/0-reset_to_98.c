4eb3dc2db26c125892cc04f38d928a70c8b66a4c
0xMidnightBlues <126298314+0xMidnightBlues@users.noreply.github.com>
2023-03-28 00:14:52 +0000

    test 1

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - takes an int pointer and reset its values to 98
+ *
+ * Return: northing
+ */
+
+void reset_to_98(int *n)
+{
+    *n = 98
+}
