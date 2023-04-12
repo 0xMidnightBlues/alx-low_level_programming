93db24538872d8ce39028c9f476b8ac84e02fb2c
0xMidnightBlues <ataleb.codes@gmail.com>
2023-04-12 09:23:55 -0700

    betty

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -12,10 +12,10 @@ char *_strdup(char *str)
 	char    *ret;
 	int     i = 0;
 
-    if (!str)
-    {
-        return (NULL);
-    }
+	if (!str)
+	{
+		return (NULL);
+	}
 	while (str[i] != '\0')
 		i++;
 	ret = malloc(sizeof(char) * i + 1);
