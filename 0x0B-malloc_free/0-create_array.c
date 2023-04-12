555ac7b6f1c835a568d821907634b881e4403f2f
0xMidnightBlues <ataleb.codes@gmail.com>
2023-04-12 09:36:47 -0700

    1

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,7 +15,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	array = malloc(sizeof(char) * size + 1);
+	array = malloc(sizeof(char) * size);
 	if (!array)
 		return (NULL);
 	while (i < size)
