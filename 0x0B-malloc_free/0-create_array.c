d540e86adeac6d90419fb4dfe20dd8a1bea0383d
0xMidnightBlues <126298314+0xMidnightBlues@users.noreply.github.com>
2023-04-12 16:22:29 +0000

    fix:

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,11 +10,11 @@
 
 char *create_array(unsigned int size, char c)
 {
+	unsigned    int i = 0;
+	char            *array;
+
 	if (size == 0)
 		return (NULL);
-	char            *array;
-	unsigned    int i = 0;
-
 	array = malloc(sizeof(char) * size + 1);
 	if (!array)
 		return (NULL);
