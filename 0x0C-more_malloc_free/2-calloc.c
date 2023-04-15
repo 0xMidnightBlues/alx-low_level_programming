97d6291313b0fb57cded6148398b66c577a2626f
0xMidnightBlues <126298314+0xMidnightBlues@users.noreply.github.com>
2023-04-15 02:58:39 +0000

    changes

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -10,12 +10,21 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void	*ret;
+	void *p;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
-	ret = malloc(nmemb * size);
-	if (!ret)
+	p = malloc(nmemb * size);
+	if (p == NULL)
+	{
 		return (NULL);
-	return (ret);
+	}
+
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		*((char *)(p) + i) = 0;
+	}
+
+	return (p);
 }
