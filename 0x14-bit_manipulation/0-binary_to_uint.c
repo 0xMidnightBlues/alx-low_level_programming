d91aaa78153e322d4860b69ffb316368e2ccd454
0xMidnightBlues <126298314+0xMidnightBlues@users.noreply.github.com>
2023-05-04 22:06:49 +0000

    changed betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * binary_to_ret - function that converts a binary number to an unsigned int.
+ * binary_to_uint - function that converts a binary number to an unsigned int.
  * @b: pointer to the string of 0's and 1's
  * Return: an unsigned int number
  */
